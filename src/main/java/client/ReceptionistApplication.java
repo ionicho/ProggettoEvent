f6ab4381e84758bf8018027e4be01e1262eda90a
Gaetano Comandatore <g.comandatore@studenti.unibg.it>
2024-01-10 22:46:23 +0100

    Anche Room risponde al client ... da sistemare

--- src/main/java/client/ReceptionistApplication.java
+++ src/main/java/client/ReceptionistApplication.java
@@ -3,13 +3,18 @@ package client;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import server.model.Event;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import server.model.*;
+import server.service.LocalDateTypeAdapter;
+
+import java.time.LocalDate;
 
 import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.*;
 
 /**
  * Main del client utilizzato dal Receptionist
@@ -21,6 +26,8 @@ public class ReceptionistApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    	
+    	// finestra per l'evento
         primaryStage.setTitle("Ricerca Evento");
 
         TextField idField = new TextField();
@@ -39,13 +46,45 @@ public class ReceptionistApplication extends Application {
         Scene scene = new Scene(vbox, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
+        // finestra per la camera
+
+        Stage roomStage = new Stage();
+        roomStage.setTitle("Ricerca Camera");
+
+        TextField roomIdField = new TextField();
+        roomIdField.setPromptText("Inserisci l'ID della camera");
+
+        TextField roomResponseField = new TextField();
+        roomResponseField.setEditable(false);
+
+        Button roomSearchButton = new Button("Cerca");
+        roomSearchButton.setOnAction(e -> searchRoom(roomIdField.getText(), roomResponseField));
+
+        VBox roomVbox = new VBox(roomIdField, roomSearchButton, roomResponseField);
+        roomVbox.setPadding(new Insets(10));
+        roomVbox.setSpacing(8);
+
+        Scene roomScene = new Scene(roomVbox, 300, 200);
+        roomStage.setScene(roomScene);
+        roomStage.show();
+    }
     private void searchEvent(String id, TextField responseField) {
         String url = "http://localhost:8080/api/eventi/" + id;
         Event evento = restTemplate.getForObject(url, Event.class);
         responseField.setText(evento.toString()); // Mostra la risposta del server nel campo di testo
     }
+    
+    private void searchRoom(String nome, TextField responseField) {
+        String url = "http://localhost:8080/api/room/" + nome;
+        // Crea un'istanza di Gson registrando il TypeAdapter per LocalDate
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter()).create();
+        // Deserializza la JSON data in un ResourceRoom
+        ResourceRoom room = gson.fromJson(restTemplate.getForObject(url, String.class), ResourceRoom.class);
+        // Mostra la risposta del server nel campo di testo
+        responseField.setText("Nome: " + room.getName() + "\nCosto: " + room.getCosto() + "\nN. letti: " + room.getNLetti() + "\nTipo: " + room.getType());
+    }
+
 
     public static void main(String[] args) {
         launch(args);
