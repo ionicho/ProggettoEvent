ededf491754c6b6c7962b8eb1ac345a2bb961efb
Gaetano Comandatore <g.comandatore@studenti.unibg.it>
2024-01-15 22:42:10 +0100

    W(indows)Room2 con colori e trigger di eventi

--- src/main/java/client/ReceptionistApplication.java
+++ src/main/java/client/ReceptionistApplication.java
@@ -1,24 +1,10 @@
 package client;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
-
-import server.model.*;
-import server.service.LocalDateTypeAdapter;
-
-import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import com.google.gson.*;
-
+import client.Windows.*;
 
 /**
  * Main del client utilizzato dal Receptionist
@@ -30,68 +16,21 @@ public class ReceptionistApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-    	
-    	// finestra per l'evento
+        // finestra per l'evento
         Stage eventStage = new Stage();
-        eventStage.setTitle("Ricerca Evento");
-
-        TextField eventIdField = new TextField();
-        eventIdField.setPromptText("Inserisci l'ID dell'evento");
-
-        TextField eventResponseField = new TextField();
-        eventResponseField.setEditable(false); // Impedisce all'utente di modificare il campo di risposta
-
-        Button eventSearchButton = new Button("Cerca");
-        eventSearchButton.setOnAction(e -> searchEvent(eventIdField.getText(), eventResponseField));
-
-        VBox eventVbox = new VBox(eventIdField, eventSearchButton, eventResponseField);
-        eventVbox.setPadding(new Insets(10));
-        eventVbox.setSpacing(8);
-
-        Scene eventScene = new Scene(eventVbox, 300, 200);
-        eventStage.setScene(eventScene);
-        eventStage.show();
+        WEvent wEvent = new WEvent();
+        wEvent.start(eventStage, restTemplate);
 
         // finestra per la camera
-
         Stage roomStage = new Stage();
-        roomStage.setTitle("Ricerca Camera");
-
-        TextField roomIdField = new TextField();
-        roomIdField.setPromptText("Inserisci l'ID della camera");
-
-        TextField roomResponseField = new TextField();
-        roomResponseField.setEditable(false);
-
-        Button roomSearchButton = new Button("Cerca");
-        roomSearchButton.setOnAction(e -> searchRoom(roomIdField.getText(), roomResponseField));
-
-        VBox roomVbox = new VBox(roomIdField, roomSearchButton, roomResponseField);
-        roomVbox.setPadding(new Insets(10));
-        roomVbox.setSpacing(8);
-
-        Scene roomScene = new Scene(roomVbox, 300, 200);
-        roomStage.setScene(roomScene);
-        roomStage.show();
-    }
-    private void searchEvent(String id, TextField responseField) {
-        String url = "http://localhost:8080/api/eventi/" + id;
-        System.out.printf("\n\n%s\n\n", url);
-        Event evento = restTemplate.getForObject(url, Event.class);
-        responseField.setText(evento.toString()); // Mostra la risposta del server nel campo di testo
-    }
-    
-    private void searchRoom(String nome, TextField responseField) {
-        String url = "http://localhost:8080/api/room/" + nome;
-        System.out.printf("\n\n%s\n\n", url);
-        ResourceRoom room = restTemplate.getForObject(url, ResourceRoom.class);
-        //Object room = restTemplate.getForObject(url, Object.class);
+        WRoom wRoom = new WRoom();
+        wRoom.start(roomStage, restTemplate);
         
-        System.out.printf("\n\n%s\n\n", room.toString());
-        
-        responseField.setText(room.toString()); // Mostra la risposta del server nel campo di testo
-
-        }
+        // finestra di tutte le camere
+        Stage roomStage2 = new Stage();
+        WRoom2 wRoom2 = new WRoom2();
+        wRoom2.start(roomStage2, restTemplate);
+    }
    
 
     public static void main(String[] args) {
